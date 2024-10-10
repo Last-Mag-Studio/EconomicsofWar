2b1b578f7b846dac7b97415ebebbd1575a063316
Chernyy Volk <calicojones59@gmail.com>
2024-10-10 14:02:16 -0400

    bug fixes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ host: '66.179.252.151', port: 8447 });
 let connections = 0;
 let totalTimePlayed = 0;
+
+// Read totalTimePlayed from file
+const filePath = 'totalTimePlayed.txt';
+if (fs.existsSync(filePath)) {
+    totalTimePlayed = parseFloat(fs.readFileSync(filePath, 'utf8')) || 0;
+}
 
 wss.on('connection', (ws) => {
     connections++;
@@ -14,10 +21,20 @@ wss.on('connection', (ws) => {
         }
     });
 
+    ws.on('message', (message) => {
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === 'requestTotalTimePlayed') {
+            ws.send(JSON.stringify({ type: 'totalTimePlayed', data: totalTimePlayed }));
+        }
+    });
+
     ws.on('close', () => {
         connections--;
         const connectionEndTime = Date.now();
         totalTimePlayed += (connectionEndTime - connectionStartTime) / 1000; // convert to seconds
+
+        // Write totalTimePlayed to file
+        fs.writeFileSync(filePath, totalTimePlayed.toString());
 
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
@@ -35,6 +52,6 @@ setInterval(() => {
             client.send(JSON.stringify({ type: 'time', data: currentTime }));
         }
     });
-}, 15000); // 15000 ms = 15 seconds
+}, 1000); // 1000 ms = 1 second
 
 console.log('WebSocket server is running on ws://66.179.252.151:8447');
