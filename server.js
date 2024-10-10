726e38b3b7cae7b8acecb4bab88fcc184fa23953
Chernyy Volk <calicojones59@gmail.com>
2024-10-10 13:52:08 -0400

    bug fix

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ host: '66.179.252.151', port: 8447 });
 let connections = 0;
-let totalTimePlayed = 0;
 
 wss.on('connection', (ws) => {
     connections++;
-    const connectionStartTime = Date.now();
-
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify({ type: 'connections', data: connections }));
@@ -16,16 +13,21 @@ wss.on('connection', (ws) => {
 
     ws.on('close', () => {
         connections--;
-        const connectionEndTime = Date.now();
-        totalTimePlayed += (connectionEndTime - connectionStartTime) / 1000; // convert to seconds
-
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify({ type: 'connections', data: connections }));
-                client.send(JSON.stringify({ type: 'totalTimePlayed', data: totalTimePlayed }));
             }
         });
     });
 });
+
+setInterval(() => {
+    const currentTime = new Date().toLocaleTimeString();
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'time', data: currentTime }));
+        }
+    });
+}, 300000); // 300000 ms = 5 minutes
 
 console.log('WebSocket server is running on ws://66.179.252.151:8447');
