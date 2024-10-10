2426e373bca199d7e5552fa532de9a025be7996e
Chernyy Volk <calicojones59@gmail.com>
2024-10-10 13:48:51 -0400

    added total time played

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ host: '66.179.252.151', port: 8447 });
 let connections = 0;
+let totalTimePlayed = 0;
 
 wss.on('connection', (ws) => {
     connections++;
+    const connectionStartTime = Date.now();
+
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify({ type: 'connections', data: connections }));
@@ -13,9 +16,13 @@ wss.on('connection', (ws) => {
 
     ws.on('close', () => {
         connections--;
+        const connectionEndTime = Date.now();
+        totalTimePlayed += (connectionEndTime - connectionStartTime) / 1000; // convert to seconds
+
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify({ type: 'connections', data: connections }));
+                client.send(JSON.stringify({ type: 'totalTimePlayed', data: totalTimePlayed }));
             }
         });
     });
