c174351a098cb42d96e10496c44ecb15b565e449
Chernyy Volk <85821992+chernyyvolk@users.noreply.github.com>
2024-10-10 17:15:36 -0400

    bug fixes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,25 @@
-ws.on('connection', (ws) => {
+const fs = require('fs');
+const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
+
+const wss = new WebSocket.Server({ host: '66.179.252.151', port: 8447 });
+let connections = 0;
+let totalTimePlayed = 0;
+const clientPlayTimes = {};
+
+// Read totalTimePlayed from file
+const totalTimePlayedFilePath = 'totalTimePlayed.txt';
+if (fs.existsSync(totalTimePlayedFilePath)) {
+    totalTimePlayed = parseFloat(fs.readFileSync(totalTimePlayedFilePath, 'utf8')) || 0;
+}
+
+// Read clientPlayTimes from file
+const clientPlayTimesFilePath = 'clientPlayTimes.json';
+if (fs.existsSync(clientPlayTimesFilePath)) {
+    Object.assign(clientPlayTimes, JSON.parse(fs.readFileSync(clientPlayTimesFilePath, 'utf8')));
+}
+
+wss.on('connection', (ws) => {
     connections++;
     const clientId = uuidv4();
     clientPlayTimes[clientId] = clientPlayTimes[clientId] || 0;
@@ -8,7 +29,7 @@ ws.on('connection', (ws) => {
 
     ws.send(JSON.stringify({ type: 'clientId', data: clientId }));
 
-    ws.clients.forEach(client => {
+    wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify({ type: 'connections', data: connections }));
         }
@@ -29,10 +50,21 @@ ws.on('connection', (ws) => {
 
     ws.on('close', () => {
         connections--;
-        ws.clients.forEach(client => {
+        wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify({ type: 'connections', data: connections }));
             }
         });
     });
 });
+
+setInterval(() => {
+    const currentTime = new Date().toLocaleTimeString();
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'time', data: currentTime }));
+        }
+    });
+}, 1000);
+
+console.log('WebSocket server is running on ws://localhost:8447');
